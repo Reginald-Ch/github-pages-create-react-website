eca0f7924b19172c8f6af7919e53212ee72070ad
Reginald-ch <konovinternational@gmail.com>
2020-05-06 05:50:14 +0000

    first commit.

---
+++ src/Components/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Home.css'
+function Home() {
+    return (
+<React.Fragment>
+
+            <h1 className="textsection">Hi Buddy</h1>
+            <center>
+            <span className="emoji" role="img" aria-label="handwave">👋 </span>
+            </center>
+            <p className="text">Here to text you wellness tips<br></br> and positive messages</p>
+            <p className="section">Tap here or WhatsApp/ Text</p>
+            <p className="sector">Hi to 0208741417 </p>   
+               
+</React.Fragment>               
+                
+    )
+}
+
+export default Home
+
+
+
+
+
+
